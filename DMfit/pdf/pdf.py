bf76dfd91509fb709e5839085eebd3fc5879ca93
zemrude <zemrude@gmail.com>
2020-10-30 13:58:39 +0100

    Puttin the Parameter class inside the same module pdf

--- DMfit/pdf/pdf.py
+++ DMfit/pdf/pdf.py
@@ -4,7 +4,84 @@ import numpy as np
 import collections
 import itertools 
    
-from .parameter import Parameter    
+
+class Parameter():
+    """ Parameter class """
+    def __init__(self, name, value, limits, fixed, **kwargs):
+        
+        limits = np.asarray(limits)
+        self.limits = limits
+        self.fixed = fixed
+        self._meta_data = kwargs.copy()
+        self.value = value
+        self.name = name
+    @property    
+    def meta_data(self) -> dict:
+        return self._meta_data
+    
+    @property
+    def name(self) -> Optional[str]:
+        
+        return self._meta_data.get("name", None)
+    
+    @name.setter
+    def name(self, value: str):
+        self._meta_data["name"] = str(value)
+    
+    @property
+    def value(self) -> float:
+        """Name of the histogram (stored in meta-data)."""
+        return self._value
+    
+    @value.setter
+    def value(self, value: float):
+        """Name of the histogram (stored in meta-data)."""
+        
+        if self.limits[0] <= value <= self.limits[1]:
+            self._value = value
+        else:
+            raise ValueError(" Value {} is not between limits ({}, {})".format(value, self.limits[0], self.limits[1]))
+   
+    @property
+    def limits(self) -> np.ndarray:
+        return self._limits
+    
+    @limits.setter
+    def limits(self, limits: np.ndarray):
+        if ( len(limits) != 2 ):
+            raise ValueError( "Limits need a dim = 2")
+        elif limits[0] >= limits[1]:
+            raise ValueError( "Lower limit is equal or greater than upper limit")
+        else:
+            self._limits = limits
+    @property
+    def upper_limit(self) -> float:
+        return self._limits[1]
+    @property
+    def lower_limit(self) -> float:
+        return self._limits[0]
+    @upper_limit.setter
+    def upper_limit(self, value : float):
+        if value < self._limits[0]:
+            raise ValueError( " Upper limit {} is smaller than lower limit {}".format(value, self._limits[0]))
+        else:
+            self._limits[1] = value
+    @lower_limit.setter
+    def lower_limit(self, value : float):
+        if value > self._limits[1]:
+            raise ValueError( " Lower limit {} is greater than upper limit {}".format(value, self._limits[1]))
+        else:
+            self._limits[0] = value
+    
+        
+    def __str__(self):
+        lines = []
+        lines.append(" Name: {}".format(str(self.name)))
+        lines.append(" Value: {}".format(str(self._value)))
+        lines.append(" Limits: ({}, {})".format(str(self.limits[0]), str(self.limits[1])))
+        lines.append(" Is fixed? {}".format(self.fixed))
+        return "\n".join(lines)
+
     
 class PdfBase(abc.ABC):
     """PDF base class"""
@@ -186,13 +263,32 @@ class Model():
         expression = self.expression
         variables = {"index" : index, "self": self}
         return eval(expression, {}, variables)
-            
+    def __mul__(self, other):
+        """If we multiply by a float or a int, the method returns simply the frequencies multiplied """
+        
+        if isinstance(other, Parameter):
+            expression = "self._parameters['{}'].value * {}".format(other.name, self.expression)
+            name = "{}*{}".format(other.name, self.name)
+            pdfs = list(self._pdfs.values())
+            param_self = list(self._parameters.values())
+            param = list(itertools.chain(param_self, [other]))
+            m = Model(pdfs = pdfs, parameters= param, name=name, expression=expression)
+            return m
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    
+    
     def __add__(self, other):
         
         if isinstance(other, PdfBase):
             expression = "{} + self._pdfs['{}'][index]".format(self.expression, other.name)
             name = "{} + {}".format(self.name, other.name)
-            m = Model(pdfs=[list(self._pdfs.values()) + other], parameters=list(self._parameters.values()), name=name, expression=expression)
+            pdfs_self = list(self._pdfs.values())
+            pdfs = list(itertools.chain(pdfs_self, other))
+            
+            m = Model(pdfs=pdfs, parameters=list(self._parameters.values()), name=name, expression=expression)
             return m
         
         elif isinstance(other, Model):
